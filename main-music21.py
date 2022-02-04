d2d14f193c646c98da7c363f366ff2b25b46c6f3
Paulymorphism <48162037+Paulymorphism@users.noreply.github.com>
2022-02-04 15:49:40 -0800

    DONE!

--- main-music21.py
+++ main-music21.py
@@ -11,8 +11,9 @@ import keyboard
 
 from mido import MidiFile
 # from chordgeneration import convert # Ryan's file
-# import bassGenerator # Juhan's file
 from pauls_melody import Paul_melody
+from Bass_Generator import BassGenerator
+from Chord_Generation import Chord_Generator
 
 
 def main():
@@ -31,9 +32,16 @@ def main():
     generatedmusic = stream.Stream()
     # keeps track of how many measures are played
     measures_played = 0
+    chords = {
+        "I": ('C', ['C', 'E', 'G']),
+        "vi": ('Am', ['A', 'C', 'E']),
+        "IV": ('F', ['F', 'A', 'C']),
+        "V": ('G', ['G', 'B', 'D']),
+    }
 
     # generate the first 8 measures
-    generate()
+    cg = Chord_Generator(corpus=None, chord=chords, seed=1)
+    generate(cg)
     mf = midi.MidiFile()
     mf.open(music_file)
     mf.read()
@@ -46,7 +54,8 @@ def main():
         start_time = time.time()
         print("current measure {}".format(measures_played))
         measures_played += measures
-        generate()
+        cg = Chord_Generator(corpus=None, chord=chords, seed=1)
+        generate(cg)
         mf = midi.MidiFile()
         mf.open(music_file)
         mf.read()
@@ -57,15 +66,15 @@ def main():
         sp.stop()
 
 
-def generate():
+def generate(cg):
     # call functions
-    chord = Ryan_chord()
+    chord = Ryan_chord(cg)
     Paul_melody(chord, 32)
     Junhan_bass()
 
     # merge midifiles
     cv1 = MidiFile('Midi_outputs/melody.midi', clip=True)
-    cv2 = MidiFile('Midi_outputs/bass.mid', clip=True)
+    cv2 = MidiFile('Midi_outputs/chordAndBass.mid', clip=True)
 
     cv1.tracks.append(cv2.tracks[1])
     cv1.tracks.append(cv2.tracks[2])
@@ -73,26 +82,16 @@ def generate():
     cv1.save('8bars.midi')
 
 
-def Ryan_chord():
-    print("Ryan")
-    # chords = {
-    #     "I"     : ('C', ['C', 'E', 'G']),
-    #     "vi"     : ('Am', ['A', 'C', 'E']),
-    #     "IV"     : ('F', ['F', 'A', 'C']),
-    #     "V"     : ('G', ['G', 'B', 'D']),
-    # }
-    # cg = Chord_Generator(corpus=None, chord=chords)
-    # name, matrix = cg.generate_chord_progression()
-    # print(name)
-    # cg.convert_to_midi(name)
-    # return name
-
-    return ['F', 'A', 'G', 'C', 'B', 'E', 'A', 'D', 'G', 'A', 'F', 'G', 'D', 'G', 'C', 'C', 'F', 'A', 'G', 'C', 'B', 'E', 'A', 'D', 'G', 'A', 'F', 'G', 'D', 'G', 'C', 'C']
+def Ryan_chord(chord_prog):
+    name, matrix = chord_prog.generate_chord_progression()
+    length = chord_prog.convert_to_midi(name)
+    return name
 
 
 def Junhan_bass():
-    print("Junhan")
-    # bassGenerator.generate("Midi_outputs/chord.mid", "Midi_outputs/chordAndBass.mid")
+    a = BassGenerator()
+    a.generate("Midi_outputs/chord.mid",
+               "Midi_outputs/chordAndBass.mid")
 
 
 if __name__ == '__main__':
